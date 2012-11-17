bc53ae1f594366b477653149e10e9594775433d1
Ubuntu-mini-stephen <sjdixon2@gmail.com>
2012-11-16 23:00:28 -0700

    Committing changes for deployment on ec2 - sjdixon2@gmail.com

---
+++ src/networking/Client.java
@@ -0,0 +1,106 @@
+package networking;
+
+import org.zeromq.ZMQ;
+
+import algorithms.IAlgorithm;
+import algorithms.LuckysAlgorithm;
+
+import data.Chromosome;
+import data.IChromosome;
+import data.IWell;
+import data.Well;
+
+final class Client
+{
+	public static void main(String[] args)
+	{
+		Client.run(args);
+	}
+
+	public static void run(String[] args)
+	{
+		String teamName = null;
+		String matchToken = null;
+		String teamPassword = null;
+		String hostName = null;
+
+		if(args.length < 4) {
+			Client.printHelp();
+			return;
+		}
+
+		teamName = args[0];
+		teamPassword = args[1];
+		matchToken = args[2];
+		hostName = args[3];
+		Communication.setHostName(hostName);
+
+		System.out.println("Starting Battle Tetris Client...");
+
+		Command command = new Command();
+
+		// retrieve the command to connect to the server
+		String connectCommand = command.getMatchConnectCommand(teamName, matchToken, teamPassword);
+
+		// retrieve the communication singleton
+		Communication comm = Communication.getInstance();
+		comm.setSubscription(matchToken);
+
+		// send the command to connect to the server
+		System.out.println("Connecting to server...");
+		String clientToken = comm.send(connectCommand, Command.Key.CLIENT_TOKEN);
+		System.out.println("Received client token... " + clientToken);
+
+		// the GameInfo object will hold the client's name, token, game type, etc.
+		GameInfo gameInfo = new GameInfo(clientToken, teamName);
+
+		///
+		/// ADD ALGORITHM
+		int numGames = 19;
+		int numGamesWon = 0;
+		int numGamesLost = 0;
+		for(int game=0; game < numGames; game++){
+			initializeAlgorithm();
+			boolean gameOver = false;
+			while(!gameOver){
+				// Receive messages, send moves
+				
+			}
+		}
+		///
+
+		System.out.println("Exiting...");
+		System.out.println("Games Won: " + numGamesWon);
+		System.out.println("Games Lost: " + numGamesLost);
+	}
+
+	public static void printHelp()
+	{
+			System.out.println("usage: Client <team-name> <team-password> <match-token> <host-name>");
+	}
+	
+	protected static IWell well;
+	protected static final int HEIGHT=20, WIDTH=10;
+	protected static IAlgorithm algorithm;
+	protected static IChromosome chromosome;
+	
+	public static void initializeAlgorithm(){
+		well = new Well(HEIGHT, WIDTH);
+		chromosome = createChromosome();
+		algorithm = new LuckysAlgorithm(well, chromosome);
+		
+	}
+	
+	public static IChromosome createChromosome(){
+		double[] coeffs = new double[6];
+		coeffs[IChromosome.HEIGHT] = -8;
+		coeffs[IChromosome.HOLE] = -1;
+		coeffs[IChromosome.BLOCKADE] = -0.5;
+		coeffs[IChromosome.FLOOR] = -0.3;
+		coeffs[IChromosome.ROWCLEAR] = 8;
+		coeffs[IChromosome.WALL] = 0;
+		IChromosome chro = new Chromosome(coeffs);
+		return chro;
+	}
+	
+}
---
+++ src/networking/Command.java
@@ -0,0 +1,51 @@
+package networking;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+final class Command
+{
+	class Key {
+		private static final String TEAM_NAME = "team_name";
+		public static final String CLIENT_TOKEN = "client_token";
+		private static final String COMM_TYPE = "comm_type";
+		private static final String MATCH_TOKEN = "match_token";
+		private static final String PASSWORD = "password";
+
+		class CommType {
+			private static final String MATCH_CONNECT = "MatchConnect";
+			private static final String GAME_MOVE = "GameMove";
+		}
+	}
+
+	public String getMatchConnectCommand(String teamName, String matchToken, String teamPassword)
+	{
+		JSONObject json = new JSONObject();
+
+		try {
+			json.put(Key.COMM_TYPE, Key.CommType.MATCH_CONNECT);
+			json.put(Key.TEAM_NAME, teamName);
+			json.put(Key.MATCH_TOKEN, matchToken);
+			json.put(Key.PASSWORD, teamPassword);
+		} catch(JSONException e) {
+			System.err.println("[Command connectCommand] couldn't create command");
+			return null;
+		}
+
+		return json.toString();
+	}
+	
+	public String createGameMoveCommand(String path){
+		JSONObject json = new JSONObject();
+		try{
+			json.put(Key.COMM_TYPE, Key.CommType.GAME_MOVE);
+		}
+		catch(JSONException e){
+			
+		}
+		String gameMove = json.toString();
+		return gameMove;
+	}
+	
+	
+}
